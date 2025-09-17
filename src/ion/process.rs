2988a1dbaeb75654e340db0ee247121afdd246e6
Andrew Brown <andrew.brown@intel.com>
2025-09-17 10:37:25 -0700

    Use the merged `Requirement` for limiting iteration

--- src/ion/process.rs
+++ src/ion/process.rs
@@ -1078,14 +1078,14 @@ impl<'a, F: Function> Env<'a, F> {
                 + bundle.index();
 
             self.ctx.output.stats.process_bundle_reg_probe_start_any += 1;
-            for preg in RegTraversalIter::new(
-                self.env,
-                class,
-                fixed_preg,
-                hint_reg,
-                scan_offset,
-                self.bundles[bundle].limit,
-            ) {
+            let limit = if let Requirement::Range(r) = req {
+                Some(r as usize)
+            } else {
+                None
+            };
+            for preg in
+                RegTraversalIter::new(self.env, class, fixed_preg, hint_reg, scan_offset, limit)
+            {
                 self.ctx.output.stats.process_bundle_reg_probes_any += 1;
                 let preg_idx = PRegIndex::new(preg.index());
                 trace!("trying preg {:?}", preg_idx);
